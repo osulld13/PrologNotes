16dac23f20ab3858fcbf1c973ef601ad06cd2093
Donal <osulld13@tcd.ie>
2014-11-24 18:38:50 +0000

    Commit

--- NotesChapter6.pl
+++ NotesChapter6.pl
@@ -1,4 +1,11 @@
-More Lists
+% More Lists
 
-	append(List1, List2, [List1|List2])
-	append(List1, List2, List3):- append(List1, List2, List3).
+	append([],L,L).
+	append([H|T],L2,[H|L3]) :- append(T,L2,L3).
+
+% Reversing a List
+
+	accumReverse([], L, L).
+	accumReverse([H|T], List2, Rev):- accumReverse(T, [H|List2], Rev).
+	
+	rever(List1, List2) :- accumReverse(List1, [], List2).
