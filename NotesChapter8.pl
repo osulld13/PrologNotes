519aace673cf3d547febe7a210abded86662ff64
Donal O Sullivan <osulld13@github.com>
2014-11-25 15:23:01 +0000

    Commit

--- NotesChapter8.pl
+++ NotesChapter8.pl
@@ -2,28 +2,43 @@
 
 % Extra Arguments
 
-	s --> np(subject),vp.
+	s 				--> 	np(subject),vp.
 
-	np(_) --> det,n.
-	np(X) --> pro(X).
+	np(_) 			--> 	det,n.
+	np(X) 			--> 	pro(X).
 
-	vp --> v,np(object).
-	vp --> v.
+	vp 				--> 	v,np(object).
+	vp 				--> 	v.
 
-	det --> [the].
-	det --> [a].
+	det 			--> 	[the].
+	det 			--> 	[a].
 
-	n --> [woman].
-	n --> [man].
+	n 				--> 	[woman].
+	n 				--> 	[man].
 
-	pro(subject) --> [he].
-	pro(subject) --> [she].
-	pro(object) --> [him].
-	pro(object) --> [her].
+	pro(subject) 	--> 	[he].
+	pro(subject) 	--> 	[she].
+	pro(object) 	--> 	[him].
+	pro(object) 	--> 	[her].
 
-	v --> [shoots].
+	v 				--> 	[shoots].
 
 % Here weve added pronouns to state which pronouns can occur in the subject position
 % and which ones can occur in the object position.
 
-	
+% Building Parse Trees
+
+	s(s(NP,VP)) 	--> 	np(NP),vp(VP).
+
+    np(np(DET,N)) 	--> 	det(DET),n(N).
+
+    vp(vp(V,NP)) 	--> 	v(V),np(NP).
+	vp(vp(V))		--> 	v(V).
+
+	det(det(the)) 	--> 	[the].
+	det(det(a))		--> 	[a].
+
+	n(n(woman)) 	--> 	[woman].
+	n(n(man))		--> 	[man].
+
+	v(v(shoots)) 	--> 	[shoots].
