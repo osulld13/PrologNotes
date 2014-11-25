dd640dd0ccfb7217c732d86952ea745fa75cda8e
Donal O Sullivan <osulld13@github.com>
2014-11-25 13:42:22 +0000

    Commit

--- NotesChapter8.pl
+++ NotesChapter8.pl
@@ -1,2 +1,27 @@
 % More Definite clause grammars
 
+% Extra Arguments
+
+	s --> np(subject),vp.
+
+	np(_) --> det,n.
+	np(X) --> pro(X).
+
+	vp --> v,np(object).
+	vp --> v.
+
+	det --> [the].
+	det --> [a].
+
+	n --> [woman].
+	n --> [man].
+
+	pro(subject) --> [he].
+	pro(subject) --> [she].
+	pro(object) --> [him].
+	pro(object) --> [her].
+
+	v --> [shoots].
+
+% Here weve added pronouns to state which pronouns can occur i the subject position
+% and which ones can occur in the object position. 
